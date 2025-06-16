80bc2966cf222cd98ca408f18b3127dd55697267
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-06-16 12:48:47 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,76 @@
+// for loop
+
+// public class loops {
+//     public static void main(String args[]){
+//         for(int i=0; i<11; i++){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+
+//  while loop
+
+// public class loops{
+//     public static void main(String args[]){
+
+//         int i=0;
+//         while(i<11){
+//             System.out.println(i);
+//             i++;
+//         }
+//     }
+// }
+
+// do while loop
+
+// public class loops{
+//     public static void main(String args[]){
+//         int i=0;
+//         do{
+//             System.out.println(i);
+//             i++;
+//         }
+//         while(i<11);
+//     }
+// }
+
+
+// print the sum of the first n natural numbers.
+
+// import java.util.Scanner;
+// public class loops{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the first n natural number:");
+//         int n = sc.nextInt();
+//         int sum = 0;
+//         for(int i=1; i<=n; i++){
+//             sum = sum+i;
+//         }
+//         System.out.println(sum);
+//     }
+// }
+
+
+// print the table of a number input by the user.
+
+// public class loops{
+//     public static void main(String args[]){
+//         int number = 2;
+//         for(int i=1; i<11; i++){
+//             System.out.println(number*i);
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
